b04da9f5f99f4eea7943a1ffe24531d2026713dc
satyamambast <satyamambast@github.com>
2020-11-27 01:42:07 +0530

    almost

--- maroon.py
+++ maroon.py
@@ -2,13 +2,23 @@ import discord
 import numpy as np
 import cv2
 from random import randint
+from discord.ext import commands
 import pickle
 import os
+def createimage(lower,upper,filename):
+    a=np.full((400,400,3),(randint(lower,upper), randint(200,255), randint(200,255)))
+    cv2.imwrite(filename,a)
+    cv2.imread(filename)
+    img=cv2.cvtColor(img,cv2.COLOR_HSV2BGR)
+    cv2.imwrite(filename,img)
+
 f=open("tokens.bin",'rb')
 tokens=pickle.load(f)
 TOKEN = tokens['maroon']
 
 client = discord.Client()
+
+
 
 @client.event
 async def on_message(message):
@@ -22,9 +32,7 @@ async def on_message(message):
         await message.channel.send('maroon', file=discord.File('maroon.png'))
         os.remove("maroon.png")
     if message.content.startswith('!yellow'):
-        num=randint(210,255)
-        a=np.full((400,400,3),(randint(0,200),num,num))
-        cv2.imwrite("yellow.png",a)
+        createimage(21,40,"yellow.png")
         await message.channel.send('yellow', file=discord.File('yellow.png'))
         os.remove("yellow.png")
     if message.content.startswith('!blue'):
--- test.py
+++ test.py
@@ -1,5 +1,32 @@
 import numpy as np
 import cv2
 from random import randint
-a=np.full((400,400,3),(0,0,randint(110,130)))
-cv2.imwrite("maroon.png",a)
+import PIL
+lower_blue = np.array([randint(50,70), randint(100,255), randint(100,255)])
+
+lower_blue.astype('uint8')
+print(lower_blue.dtype)
+a=np.full((400,400,3),(30, randint(200,255), randint(200,255)))
+# b=cv2.cvtColor(lower_blue,cv2.COLOR_HSV2BGR)
+# print(b)
+#cv2.imshow('before',a)
+cv2.imwrite("test.png",a)
+img=cv2.imread('test.png')
+img=cv2.cvtColor(img,cv2.COLOR_HSV2BGR)
+cv2.imwrite('test2.png',img)
+
+cv2.imshow('after',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# import numpy as np
+# import cv2
+
+# green = np.uint8([[[0, 255, 0]]]) #here insert the bgr values which you want to convert to hsv
+# hsvGreen = cv2.cvtColor(green, cv2.COLOR_BGR2HSV)
+# print(hsvGreen)
+
+# lowerLimit = hsvGreen[0][0][0] - 10, 100, 100
+# upperLimit = hsvGreen[0][0][0] + 10, 255, 255
+
+# print(upperLimit)
+# print(lowerLimit)
