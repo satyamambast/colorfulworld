fe4793a2cbbf3509925785faf7e402e9495ffaf1
satyamambast <satyam_ambast@srmuniv.edu.in>
2020-12-12 03:31:03 +0530

    actually rainbow

--- maroon.py
+++ maroon.py
@@ -5,12 +5,17 @@ from random import randint
 from discord.ext import commands
 import pickle
 import os
+import re
 def createimage(lower,upper,filename):
     a=np.full((400,400,3),(randint(lower,upper), randint(200,255), randint(200,255)))
     cv2.imwrite(filename,a)
     img=cv2.imread(filename)
     img=cv2.cvtColor(img,cv2.COLOR_HSV2BGR)
     cv2.imwrite(filename,img)
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return  tuple([int(value[i:i+lv//3], 16) for i in range(0, lv, lv//3)][::-1])
 
 f=open("tokens.bin",'rb')
 tokens=pickle.load(f)
@@ -39,6 +44,19 @@ async def blue(ctx):
     await ctx.send('blue', file=discord.File('blue.png'))
     os.remove("blue.png")
 
+@client.command(aliases=['color','clr'])
+async def colour(ctx,hex):
+    if re.match(r'^#{0,1}[0-9A-F]{6}$',hex,re.I):
+        clr=hex_to_rgb(hex)
+        a=np.full((400,400,3),clr)
+        cv2.imwrite("colour.png",a)
+        await ctx.send(hex, file=discord.File('colour.png'))
+        os.remove("colour.png")
+    else:
+        await ctx.send("Invalid Hex : Please only enter six digit hex with a single #")
+        
+
+        
 @client.event
 async def on_ready():
     print('Logged in as')
